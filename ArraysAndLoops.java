2a1f03ad58741bc7bf1e312d9e9b5f8ad0fed54c
randy <randyr_1993@yahoo.com>
2022-10-06 01:14:43 -0400

    Arrays And Loops Practice

--- ArraysAndLoops.java
+++ ArraysAndLoops.java
@@ -12,4 +12,4 @@ public class ArraysAndLoops {
 //Loops can run through elements in an array 
 //If you try to go over the length of the array, you will get an index out of bounds exception error
 //bc there is no element found
-//Loop should only run as long as "i" is smaller than the array length
+//Loop should only run as long as "i" is smaller than the array

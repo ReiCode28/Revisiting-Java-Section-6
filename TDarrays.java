baf3a420654f4d8908b147fb58605b2367c75bcb
randy <randyr_1993@yahoo.com>
2022-10-16 15:46:54 -0400

    Looped a 2D Array

--- TDarrays.java
+++ TDarrays.java
@@ -3,10 +3,26 @@ import java.util.Arrays;
 public class TDarrays {
     public static void main(String[] args) {
         int[][] grades = {
-            {72, 74, 78, 76},
-            {65, 64, 61, 67},
-            {95, 98, 99, 100}
+            {72, 74, 78, 76}, //Harry
+            {65, 64, 61, 67}, //Ron
+            {95, 98, 99, 100} //Hermione
         };
+
+
+        for(int i = 0; i < grades.length; i++){
+            
+            switch (i){
+                case 0: System.out.print("\tHarry: "); break;
+                case 1: System.out.print("\tRon: "); break;
+                case 2: System.out.print("\tHermione: "); break;
+            }
+            for(int j = 0; j < grades[i].length; j++){
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+
         // grades[0][0] = 72;
         // grades[0][1] = 74;
         // grades[0][2] = 78;
@@ -22,9 +38,9 @@ public class TDarrays {
         // grades[2][2] = 99;
         // grades[2][3] = 100;
 
-        System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
-        System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
-        System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+        // System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+        // System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
+        // System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
     }
 }
 
